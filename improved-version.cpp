22e09eece663922d87fc491b3e398df196c7ee85
Ali Dalbah <70911460+Ali-Dalbah@users.noreply.github.com>
2023-06-11 01:04:21 +0300

    Update improved-version.cpp
    Solving a Bug.
    Make the particle take the backwards gap to avoid collision (making a car look behind before switching its lane to avoid accidents).

--- improved-version.cpp
+++ improved-version.cpp
@@ -5,14 +5,15 @@
 #include <string>
 #define V_MAX 5
 
-static const int l = 50000;
+static const int l = 10000;
 static float meanVTemp;
+static unsigned long long int change = 0;
 
 struct particle{
     int pos;
     int vel;
     particle(int pos, int vel): pos(pos), vel(vel) {}
-    particle(const particle& p): pos(p.pos), vel(p.pos) {}
+    particle(const particle& p): pos(p.pos), vel(p.vel) {}
     inline void move() {
         pos = (pos + vel) % l;
     }
@@ -22,6 +23,15 @@ struct particle{
     }
     inline void deaccelarate() {
         if (vel > 0) vel--;
+    }
+    inline int operator-(const particle& p) const {
+        return pos - p.pos;
+    }
+    inline bool operator>(const particle& p) const {
+        return pos > p.pos;
+    }
+    inline bool operator<(const particle& p) const {
+        return pos < p.pos;
     }
 };
 
@@ -35,19 +45,19 @@ int getAheadParticle(const particle& p, const vector& road) {
     const int goal = p.pos;
     const int size = road.size();
     if (size == 0) return -1;
-    int left = 0; int right = size - 1;
+    int left = 0; int right = size;
     int middle;
     while (left < right) {
         middle = (right + left) / 2;
-        if (road[middle].pos <= goal) {
+        if (road[middle].pos < goal) {
             left = middle + 1;
         } else {
             right = middle;
         }
     }
-    return left;
+    return left == size && road[left].pos < goal? 0: left;
 }
-// gap = ((i+1).pos - (i).pos - 1 + l)%l
+
 void doOperations(vector& thisRoad, vector& otherRoad, const float sto, const float sw) {
     int size = thisRoad.size();
     if (size == 0) {
@@ -58,22 +68,26 @@ void doOperations(vector& thisRoad, vector& otherRoad, const float sto, const fl
     for (int i = 0; i < size; i++) {
         particle& p = thisRoad[i];
         p.accelarate();
-        int gap = (thisRoad[(i + 1)%l].pos - p.pos - 1 + l) % l;
+        int gap = (thisRoad[(i + 1)%size] - p + l) % l - 1;
         if (gap < p.vel) {
             if (getRandom() < sw) {
-                //std::cout<<"x"<<std::endl;
                 int x = getAheadParticle(p, otherRoad);
-                int otherGap = x == -1? V_MAX: (otherRoad[x].pos - p.pos - 1 + l) % l;
-                if (otherGap > gap) {
+                int otherGap = x == -1? V_MAX: (otherRoad[x] - p + l) % l - 1;
+                bool backWontSlow = x == -1 || x == 0 || ((otherRoad[(x - 1)].pos + otherRoad[(x - 1)].vel) % l ) < p.pos ;
+                if (otherGap > gap && otherRoad.size() < l - 1 && backWontSlow) {
                     if (x == -1) otherRoad.emplace_back(particle(p));
                     else if (x == 0) {
-                        if (otherRoad[0].pos > p.pos) otherRoad.insert(otherRoad.begin(), particle(p));
-                        else otherRoad.emplace_back(particle(p));
+                       if (otherRoad[0] > p) {
+                        otherRoad.insert(otherRoad.begin(), particle(p));
+                       }
+                       else {
+                        otherRoad.emplace_back(particle(p));
+                       }
                     } else {
                         otherRoad.insert(otherRoad.begin() + x, particle(p));
                     }
                     thisRoad.erase(thisRoad.begin() + i);
-                    //std::cout<<"switch"<<std::endl;
+                    change++;
                     i--;
                     size--;
                     continue;
@@ -87,8 +101,13 @@ void doOperations(vector& thisRoad, vector& otherRoad, const float sto, const fl
         if (p.vel > V_MAX)
             std::cout<<p.vel<<std::endl;
     }
+    if (size > 1) {
+        if (thisRoad[size - 1].pos < thisRoad[size - 2].pos) {
+            thisRoad.insert(thisRoad.begin(), particle(thisRoad[size - 1]));
+            thisRoad.pop_back();
+        }
+    }
     meanVTemp /= size;
-    //std::cout<<meanVTemp<<std::endl;
 }
 
 void appendToFile(const char* name, const char* data) {
@@ -100,35 +119,42 @@ void appendToFile(const char* name, const char* data) {
 
 void start(const int n, const float sto, const float sw) {
     vector lane1, lane2;
-    int X = 0, Y = 0;
     for (int i = 0; i < n; i++) {
-        lane1.emplace_back(particle(X++, 0));
-        lane2.emplace_back(particle(Y++, 0));
+        if (getRandom() < 0.5f && lane1.size() < l)
+            lane1.emplace_back(particle(lane1.size(), 0));
+        else
+            lane2.emplace_back(particle(lane2.size(), 0));
     }
-    const int simulationTime = 4000;
+    const int simulationTime = 8000;
     float meanV1 = 0, meanO1 = 0, meanV2 = 0, meanO2 = 0;
+    std::cout<<"-----\t start \t-----"<<std::endl;
+    std::cout<<"n "<<n<<"\t p = "<<sto<<"\t sw = "<<sw<<std::endl;;
     int t = 0;
     while (t < simulationTime) {
         doOperations(lane1, lane2, sto, sw);
-        meanV1 = (meanVTemp + meanV1 * t) / (t + 1);
-        meanO1 = ((float)(lane1.size()) / l + meanO1 * t) / (t + 1);
+        meanV1 = meanVTemp;
+        meanO1 = (float)(lane1.size()) / l;
         doOperations(lane2, lane1, sto, sw);
-        meanV2 = (meanVTemp + meanV2 * t) / (t + 1);
-        meanO2 = ((float)(lane2.size()) / l + meanO2 * t) / (t + 1);
+        meanV2 = meanVTemp;
+        meanO2 = (float)(lane2.size()) / l;
         t++;
+        if (!(t%1000)) {
+            std::cout<<"\r"<<(float)(t)/simulationTime*100<<"%....";
+            std::cout.flush();
+        }
     }
-    std::string str = "----- Summary -----";
-    str += "\nN = " + std::to_string(n) + "\t sto = " + std::to_string(sto) + "\t sw = " + std::to_string(sw);
-    str += "\nMeanV1 "+std::to_string(meanV1)+"\tMeanO1 "+std::to_string(meanO1);
-    str += "\nMeanV2 "+std::to_string(meanV2)+"\tMeanO2 "+std::to_string(meanO2)+"\n";
+    std::string str = "N = " + std::to_string(n) + "\t sto = " + std::to_string(sto) + "\t sw = " + std::to_string(sw);
+    str += "\tMeanV1 = "+std::to_string(meanV1)+"\tMeanO1 = "+std::to_string(meanO1);
+    str += "\tMeanV2 = "+std::to_string(meanV2)+"\tMeanO2 = "+std::to_string(meanO2)+"\n";
     appendToFile("data.txt", str.c_str());
 }
 
 
 int main(int argc, char **argv) {
     std::srand(time(nullptr));
-    for (int n = 10; n < l; n+=20) {
-        start(n, 0.f, 0.5f);
-        start(n, 0.2f, 0.5f);
+    int n = argc == 2? std::stoi(argv[1]): 5;
+    for (n; n < l*3/4; n+=100) {
+        start(n, 0.3f, 0.f);
+        start(n, 0.3f, 1.f);
     }
 }
